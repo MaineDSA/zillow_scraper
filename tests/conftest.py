c61b54fbad1e46bbb0a3bddb21d3073ea1c2960c
Ramona T <ramona.t.me@pm.me>
2025-11-28 19:13:41 -0500

    remove old tests

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import pytest
-import pytest_asyncio
-
-
-def pytest_runtest_setup(item: pytest_asyncio.plugin.Coroutine) -> None:
-    if "requires_browser" in item.keywords and os.getenv("CI"):
-        pytest.skip("Browser tests not supported in CI")
